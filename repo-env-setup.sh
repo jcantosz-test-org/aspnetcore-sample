757736b796c59df4aca1c78c5d5c18bdd2e7d4ac
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2023-02-15 12:37:25 -0500

    Updates to functionality (#1)
    * Updates with new vars and fucntionality
    
    * save main updates
    
    * Fix bad copy/paste
    
    * update version to deal with no tags
    
    * matrix strat
    
    * quotes
    
    * includes -> include
    
    * to Upper
    
    * update matrix
    
    * comment out matrix item
    
    * remove other matrix

--- repo-env-setup.sh
+++ repo-env-setup.sh
@@ -2,12 +2,13 @@
 export AZURE_CLIENT_ID="<>"
 export AZURE_SUBSCRIPTION_ID="<>"
 export AZURE_TENANT_ID="<>"
-
+export ORG="<>"
+export REPO=aspnetcore-sample
 
 # Create environment and set secrest for it based on env vars with the same name on the local machine
 for i in {dev,qa,uat,prod}; do
-    gh api -X PUT repos/jcantosz-test-org/aspnetcore-sample/environments/$i
+    gh api -X PUT repos/${ORG}/${REPO}/environments/$i
     for secret in {AZURE_CLIENT_ID,AZURE_SUBSCRIPTION_ID,AZURE_TENANT_ID}; do
-        gh secret set $secret --repo jcantosz-test-org/aspnetcore-sample --app actions --env $i --body ${!secret}
+        gh secret set $secret --repo ${ORG}/${REPO} --app actions --env $i --body ${!secret}
     done
 done
